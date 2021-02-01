d7f0e54fffd1b88afb49e321fab2b4c7db9777ea
Chris Drymon <chrisdrymon@yahoo.com>
2021-01-31 19:20:18 -0600

    Placed a needed comma.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
     'Natural Language :: Greek',
     'Natural Language :: English',
     'Operating System :: Microsoft :: Windows :: Windows 10',
-    'Operating System :: MacOS :: MacOS X'
+    'Operating System :: MacOS :: MacOS X',
     'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: 3.7',
     'Topic :: Text Processing :: Linguistic'
