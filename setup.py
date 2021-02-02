183c919a95836a287832f05981c16215077982b0
Chris Drymon <chrisdrymon@yahoo.com>
2021-02-02 11:12:43 -0600

    Added case and degree models.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ classifiers = [
 
 setup(
     name='angel-tag',
-    version='0.0.4',
+    version='0.0.5',
     description='An Ancient Greek Morphology Tagger',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
@@ -29,7 +29,7 @@ setup(
     classifiers=classifiers,
     keywords=['greek', 'ancient greek', 'morphology', 'classics', 'computational linguistics'],
     packages=find_packages(),
-    python_requires='=3.7.*, =3.8.*',
+    python_requires='==3.7.*, ==3.8.*',
     install_requires=['tensorflow',
                       'numpy',
                       'gdown',
