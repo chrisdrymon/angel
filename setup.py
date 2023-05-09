4bf39462f901129518c59ac4ec565a898dc0a689
chrisdrymon <chrisdrymon@yahoo.com>
2023-05-09 23:01:28 +0100

    Updated python and tensorflow requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ classifiers = [
 
 setup(
     name='angel-tag',
-    version='0.0.9',
+    version='0.0.10',
     description='An Ancient Greek Morphology Tagger',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
@@ -29,7 +29,7 @@ setup(
     classifiers=classifiers,
     keywords=['greek', 'ancient greek', 'morphology', 'classics', 'computational linguistics'],
     packages=find_packages(),
-    python_requires='>=3.7,<3.9',
+    python_requires='>=3.7',
     install_requires=['numpy',
                       'tensorflow',
                       'gdown',
